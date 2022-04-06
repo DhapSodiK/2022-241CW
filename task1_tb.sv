efebbb025fc7038cb8ea3a3717bd440308e96027
DhapSodiK <72078014+DhapSodiK@users.noreply.github.com>
2022-04-06 17:30:49 +0100

    Final tb1
    I am hoping this works but been working on task 1 accidentaly without comitting

--- task1_tb.sv
+++ task1_tb.sv
@@ -6,13 +6,52 @@ logic a,b,c;
 //Use these two signals for the outputs
 logic [1:0] y_structural, y_other;
 
+//initiate the array
+reg[2:0] mt;  
+
 // Instantiate both UUT
 task1a u1 (y_structural, a, b, c);
 task1b u2 (y_other, a, b, c);
 
+task passed;
+	$display("passed");
+endtask : passed
+
+task failed;
+	$display("failed");
+endtask : failed
+
+//take the array and replace with values from a, b and c
+assign mt={a,b,c};
+
 initial
 begin
 	//Write your testbench here
+	mt = 3'b000;
+	#50ps; assert(y_structural==y_other) passed(); else failed();
+
+	mt = 3'b001;
+	#50ps; assert(y_structural==y_other) passed(); else failed();
+
+	mt = 3'b010;
+	#50ps; assert(y_structural==y_other) passed(); else failed();
+
+	mt = 3'b011;
+	#50ps; assert(y_structural==y_other) passed(); else failed();
+
+	mt = 3'b100;
+	#50ps; assert(y_structural==y_other) passed(); else failed();
+
+	mt = 3'b101;
+	#50ps; assert(y_structural==y_other) passed(); else failed();
+
+	mt = 3'b110;
+	#50ps; assert(y_structural==y_other) passed(); else failed();
+
+	mt = 3'b111;
+	#50ps; assert(y_structural==y_other) passed(); else failed();
+
+	$display ("Done");
 
 end
 
